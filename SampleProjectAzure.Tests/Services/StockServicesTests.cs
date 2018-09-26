b157f60e2e38b9004bba7eb3a1437b8feadd45ab
nielsdelestinne <nielsdelestinne@gmail.com>
2018-09-26 14:56:10 +0200

    initial commit

---
+++ SampleProjectAzure.Tests/Services/StockServicesTests.cs
@@ -0,0 +1,22 @@
+﻿
+using SampleProjectAzure.Services;
+using Xunit;
+
+namespace SampleProjectAzure.Tests.Services
+{
+    public class StockServicesTests
+    {
+
+        [Fact]
+        public void GivenStockId_WhenStockInformationCalled_ThenReturnInformationForThatStockId()
+        {
+            var expected = "Apple";
+            var stockService = new StockService();
+
+            var actual = stockService.StockInformation(expected);
+
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ SampleProjectAzure/Services/StockService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SampleProjectAzure.Services
+{
+    public class StockService
+    {
+        public string StockInformation(string stockId)
+        {
+            return $"Some dummy information for stock {stockId}";
+        }
+    }
+}
